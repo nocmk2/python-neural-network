da5b64734f704e90278e91db543ec3e6432ab1e5
zhangmk <51670196@qq.com>
2020-06-10 23:55:12 +0800

    initial framework and params

---
+++ main.py
@@ -0,0 +1,24 @@
+class neuralNetwork:
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        self.lr = learningrate
+        pass
+
+    def train():
+        pass
+
+    def query():
+        pass
+
+
+if __name__ == "__main__":
+    input_nodes = 3
+    hidden_nodes = 3
+    output_nodes = 3
+
+    learning_rate = 0.5
+
+    n = neuralNetwork(input_nodes, hidden_nodes, ouput_nodes, learning_rate)
